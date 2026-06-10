5c9e83b77d7bc6a341599799abd73d532d9d8bb3
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-06-09 21:13:11 -0300

    Adicionando os components e testando o projeto

--- proj-micgui/src/components/Footer/index.jsx
+++ proj-micgui/src/components/Footer/index.jsx
@@ -1,1 +1,11 @@
 import './style.css'
+
+export default function Footer(){
+    return(
+        <footer className='footer'>
+            <p>
+                &copy; {new Date().getFullYear()} - Copyright - Todos os Direitos Reservados a <br />Michell Silva Santos, Guilherme Silva de Souza
+            </p>
+        </footer>
+    )
+};
--- proj-micgui/src/components/Header/index.jsx
+++ proj-micgui/src/components/Header/index.jsx
@@ -1,1 +1,31 @@
+import { NavLink } from 'react-router-dom'
 import './style.css'
+
+
+export default function Header(){
+    return(
+        <header className='header'>
+            <div>
+                <h2>Silva Vendas</h2>
+            </div>
+
+            <nav>
+                <NavLink to="/">
+                Home
+                </NavLink>
+
+                <NavLink to="/sobre-nos">
+                Sobre Nós
+                </NavLink>
+
+                <NavLink to="/produtos">
+                Produtos
+                </NavLink>
+
+                <NavLink to="/servicos">
+                Servicos
+                </NavLink>
+            </nav>
+        </header>
+    )
+};
